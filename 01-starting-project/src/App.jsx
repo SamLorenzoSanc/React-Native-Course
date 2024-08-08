9e8944e22b0a8d4aeb107535d1d8e8e55f7b6a1e
Samuel Lorenzo Sanchez <68753976+SamLorenzoSanc@users.noreply.github.com>
2024-08-08 17:55:02 +0100

    Update

---
+++ 01-starting-project/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header/Header.jsx";
+import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from "./data.js";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
+function App() {
+
+	return (
+		<>
+      <Header />
+			<main>
+        <CoreConcepts /> 
+        <Examples />
+			</main>
+		</>
+	);
+}
+
+export default App;
